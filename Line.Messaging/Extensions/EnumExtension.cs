d7104af77fb5636b8e923dbd0be28278d509733b
andy840119 <andy840119@gmail.com>
2019-04-03 18:52:26 +0800

    adjust enum extension

--- Line.Messaging/Extensions/EnumExtension.cs
+++ Line.Messaging/Extensions/EnumExtension.cs
@@ -11,16 +11,21 @@ namespace Line.Messaging.Extensions
     {
         public static string ToEnumString<T>(this T type)
         {
+            /*
             var enumType = typeof(T);
             var name = Enum.GetName(enumType, type);
             var enumMemberAttribute =
                 ((EnumMemberAttribute[])enumType.GetField(name).GetCustomAttributes(typeof(EnumMemberAttribute), true))
                 .Single();
             return enumMemberAttribute.Value;
+            */
+
+            return type.ToString();
         }
 
-        public static T ToEnum<T>(this string str)
+        public static T ToEnum<T>(this string str) where T : struct
         {
+            /*
             var enumType = typeof(T);
             foreach (var name in Enum.GetNames(enumType))
             {
@@ -29,6 +34,10 @@ namespace Line.Messaging.Extensions
                         .GetCustomAttributes(typeof(EnumMemberAttribute), true)).Single();
                 if (enumMemberAttribute.Value == str) return (T)Enum.Parse(enumType, name);
             }
+            */
+
+            if (Enum.TryParse(str, out T result))
+                return result;
 
             //throw exception or whatever handling you want or
             return default(T);
